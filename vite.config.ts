2c3e4f54858c568c7a94fd6172754f99ba410148
saz419 <sarfaraz.ahamed@datasutram.com>
2023-08-01 15:53:15 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,65 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-let faviconURL = '/favicon.svg'
+let faviconURL = "/favicon.svg";
 
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     VitePWA({
+//       includeAssets: [faviconURL],
+//       manifest: {
+//         theme_color: '#ffffff',
+//         icons: [
+//           {
+//             src: faviconURL,
+//             sizes: '512x512',
+//             type: 'image/svg+xml',
+//             purpose: 'any maskable'
+//           },
+//           {
+//             src: faviconURL,
+//             sizes: '512x512',
+//             type: 'image/png',
+//           }
+//         ]
+//       },
+//     })
+//   ]
+// })
 
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [
+      react(),
+      VitePWA({
+        includeAssets: [faviconURL],
+        manifest: {
+          theme_color: "#ffffff",
+          icons: [
+            {
+              src: faviconURL,
+              sizes: "512x512",
+              type: "image/svg+xml",
+              purpose: "any maskable",
+            },
+            {
+              src: faviconURL,
+              sizes: "512x512",
+              type: "image/png",
+            },
+          ],
+        },
+      }),
+    ],
+    base: "/",
+  };
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      includeAssets: [faviconURL],
-      manifest: {
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: faviconURL,
-            sizes: '512x512',
-            type: 'image/svg+xml',
-            purpose: 'any maskable'
-          },
-          {
-            src: faviconURL,
-            sizes: '512x512',
-            type: 'image/png',
-          }
-        ]
-      },
-    })
-  ]
-})
+  if (command !== "serve") {
+    config.base = "/react-pwa-vite/";
+  }
+
+  return config;
+});
